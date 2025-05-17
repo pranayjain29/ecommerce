4436c1509a46d4f2938056954ae12d5d5d1eceda
Pranay Jain <pranayjain354@gmail.com>
2025-05-17 07:24:21 +0000

    V1.512

--- .firebase/ecommerce-tools-99300/functions/server.js
+++
@@ -1,4 +0,0 @@
-const { onRequest } = require('firebase-functions/v2/https');
-  const server = import('firebase-frameworks');
-  exports.ssrecommercetools99300 = onRequest({"region":"us-central1"}, (req, res) => server.then(it => it.handle(req, res)));
-  
